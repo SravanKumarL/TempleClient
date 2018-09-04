05363f9f964c07ed80f62904213e3890fe3ce27a
LADDAGIRI Sravan <sravan.laddagiri@hexagon.com>
2018-09-04 17:05:47 +0530

    Revert axios

--- src/axios/poojas.js
+++ src/axios/poojas.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.SERVER_URI ? process.env.SERVER_URI : 'http://localhost:7000',
-  // baseURL: 'https://temple-api-mongo.herokuapp.com',
+  // baseURL: process.env.SERVER_URI ? process.env.SERVER_URI : 'http://localhost:7000',
+  baseURL: 'https://temple-api-mongo.herokuapp.com',
 })
  
 export default instance;
